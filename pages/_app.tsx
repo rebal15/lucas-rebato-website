49e09bf5e9fc0996f83c07554013dcf75a27c463
Lucas Rebato <lrebato10@gmail.com>
2023-01-31 21:47:00 +0000

    GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,24 @@ import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
 import CursorContextProvider from "../context/CursorContext";
+import Script from "next/script";
 
 function App({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=G-0V5MGZY4SZ"
+            />
+            <Script id="ga-config">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                  
+                    gtag('config', 'G-0V5MGZY4SZ');
+                `}
+            </Script>
             <CursorContextProvider>
                 <Component {...pageProps} />
             </CursorContextProvider>
